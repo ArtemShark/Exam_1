0979dc40f14501404fd05210027d2ec7af20e569
Artem <artem1podderegin@gmail.com>
2022-06-04 18:22:43 +0300

    func-main

--- Exam_1/Main.cpp
+++ Exam_1/Main.cpp
@@ -1,6 +1,13 @@
 #include "Header.h"
 
 int main() {
+    double number;
 
+    cout << "Enter an expression: "; // Введите выражение 
+
+    number = example();
+
+    cout << setprecision(12) << "Calculation result: " << number << endl;
+    // setprecision - указывает сколько знаков после запетой, мы хотим видеть.
 	return 0;
 }
